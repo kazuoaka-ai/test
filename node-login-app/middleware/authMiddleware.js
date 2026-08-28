117bde91d774436a33727f8ae14af180a40e49dd
kazuoaka-ai <kazuoaka@gmail.com>
2026-08-28 14:56:19 +0900

    Delete node-login-app directory

--- node-login-app/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!req.session || !req.session.userId) {
-        return res.redirect('/login');
-    };
-    next();
-};
--- node-login-app/models/Target.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const targetSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    targetId: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('Target', targetSchema);
-
